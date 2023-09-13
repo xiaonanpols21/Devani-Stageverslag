b4b69ee5bfa9fdea2628ddbc034e819a0e34d0b8
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-09-13 16:43:26 +0200

    Upload Img en Sass klaarzetten

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
-import Image from 'next/image'
-import styles from '../styles/page.module.scss'
+import styles from '../styles/main.scss';
+
+import Image from 'next/image';
+
+import Logo from "../../public/img/logo.svg";
 
 export default function Home() {
-  return (
-    <main>
-        
-    </main>
-  )
+    return (
+        <>
+            <nav>
+                <Image
+                    src={Logo}
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"
+                />
+            </nav>
+        </>
+    )
 }
