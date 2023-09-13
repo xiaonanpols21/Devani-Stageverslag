bd4870f39a0bcd04bf6d7cdf8032bb0b8d06baa2
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-09-13 17:05:04 +0200

    Dingen fixen, is nog niet gefixt

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // ...
+    rules: {
+      'no-unused-vars': 'off', // Disable the rule
+      // Other rules...
+    },
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,18 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <head>
-            <meta charSet="UTF-8" /> 
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Devani Stageverslag</title>
-            <link rel="icon" type="image/x-icon" href="/img/logo.svg" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
-        </head>
-        <body>
-            {children}
-        </body>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Devani Stageverslag</title>
+        <link rel="icon" type="image/x-icon" href="/img/logo.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
