f946396e58d6a310a0870392abe3f2f78f379bac
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-09-15 15:53:03 +0200

    Header en hero done

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,12 @@
-import Image from 'next/image';
-import PlayIcon from "../../public/img/play.svg"
+import Hero from "@/components/Hero";
 
 export default function Home() {
     return (
-        <section className='hero'>
-            <h1>
-                <span>&nbsp;Stage</span>
-                <span>verslag</span>
-            </h1>
-
-            <h2 className='visually-hidden'>Auther</h2>
-            <ul>
-                <li>Xiao Nan Pols || 500839533</li>
-                <li>Decani || Front-end developer</li>
-            </ul>
-
-            <h2>Bedrijfsbegeleider</h2>
-            <ul>
-                <li>Emke Douwe Ottema</li>
-                <li>Webdeveloper</li>
-            </ul>
-
-            <h2>Stagebegeleider</h2>
-            <ul>
-                <li>Kevin van der Wiel</li>
-                <li>k.van.der.wiel@hva.nl</li>
-            </ul>
-
-            <h2 className='visually-hidden'>HvA</h2>
-            <ul>
-                <li>CMD Amsterdam || 2-2-24</li>
-            </ul>
-
-            <button>
-                <Image
-                    src={PlayIcon}
-                    width={70}
-                    height={70}
-                    alt="Play icoon"
-                />
-            </button>
-            
-        </section>
+        <>
+            <Hero />
+            <section className="sec">
+                <h2 className="sub-dec">Inleiding</h2>
+            </section>
+        </>
     )
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import PlayIcon from "../../public/img/play.svg"
+
+export default function Hero() {
+    return (
+        <section className='hero'>
+            <h1>
+                <span>&nbsp;Stage</span>
+                <span>verslag</span>
+            </h1>
+
+            <h2 className='visually-hidden'>Auther</h2>
+            <ul>
+                <li>Xiao Nan Pols || 500839533</li>
+                <li>Decani || Front-end developer</li>
+            </ul>
+
+            <h2>Bedrijfsbegeleider</h2>
+            <ul>
+                <li>Emke Douwe Ottema</li>
+                <li>Webdeveloper</li>
+            </ul>
+
+            <h2>Stagebegeleider</h2>
+            <ul>
+                <li>Kevin van der Wiel</li>
+                <li>k.van.der.wiel@hva.nl</li>
+            </ul>
+
+            <h2 className='visually-hidden'>HvA</h2>
+            <ul>
+                <li>CMD Amsterdam || 2-2-24</li>
+            </ul>
+
+            <button>
+                <Image
+                    src={PlayIcon}
+                    width={70}
+                    height={70}
+                    alt="Play icoon"
+                />
+            </button>
+            
+        </section>
+    )
+}
