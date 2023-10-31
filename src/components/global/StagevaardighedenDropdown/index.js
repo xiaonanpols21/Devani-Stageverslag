52a1a0afcfe662b2f19c1fb7accf70188ccae70c
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-10-31 12:56:08 +0100

    stagevaardigheden dropdown

---
+++ src/components/global/StagevaardighedenDropdown/index.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+// Import the FontAwesomeIcon component
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+    faAngleDown
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function StagevaardighedenDropdown({img, title, slug, content}) {
+    
+    return (
+        <article className="article-stagevaardigheden">
+            <details>
+                <summary className="sub-dec"> 
+                    {title}
+                    <FontAwesomeIcon
+                        icon={faAngleDown}
+                        className='i'
+                    />
+                </summary>
+                <section>
+                    <h3 className="visually-hidden">Stagevaardigheden content</h3>
+                    <Image
+                        src={img} 
+                        width={310}
+                        height={200}
+                        alt="Werkzaamheden thumbnail"
+                        id={slug}
+                    />
+                     <div className="content" dangerouslySetInnerHTML={{__html: content}}></div>
+                </section>
+            </details>
+        </article>
+
+    )
+}
